05ce75c6373e3c26eda3a860fec3ae277c567d1d
joelee <as955133@gmail.com>
2018-02-01 20:11:36 +0800

    加入專案檔案。

---
+++ LAB_CRUD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LAB_CRUD.Startup))]
+namespace LAB_CRUD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
